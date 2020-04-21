b018b51da9dc5069bc0e97860193c14fd4d31ed2
Dr.Askar96 <askar_1996@list.ru>
2020-04-21 15:00:43 +0600

    new thenme

---
+++ wp-content/themes/alfa-capital/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * alfa-capital Theme Customizer
+ *
+ * @package alfa-capital
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function alfa_capital_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'alfa_capital_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'alfa_capital_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'alfa_capital_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function alfa_capital_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function alfa_capital_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function alfa_capital_customize_preview_js() {
+	wp_enqueue_script( 'alfa-capital-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'alfa_capital_customize_preview_js' );
